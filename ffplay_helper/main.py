3efb12fc50016330f4aac1ca430fbdcf7850c957
for-the-zero <ourmicroid@outlook.com>
2023-01-10 17:27:12 +0800

    Add files via upload

--- ffplay_helper/main.py
+++ ffplay_helper/main.py
@@ -24,9 +24,15 @@ dpg.create_context()
 ffplay_dict = {"path":[True,""]}
 running_ins = " "
 
+# 运行
+def run_ffplay():
+	global running_ins
+	os.system(running_ins)
+
 # 字体
 with dpg.font_registry():
-	with dpg.font("font.otf", 20) as default_font:
+	#with dpg.font("font.otf", 20) as default_font:
+	with dpg.font("font.ttf", 17) as default_font:
 		dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
 		dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
 
@@ -52,7 +58,7 @@ with dpg.file_dialog(directory_selector=False, show=False,callback=file_path_get
 
 
 # 主界面
-with dpg.window(label="ffplay",tag="Primary Window"):
+with dpg.window(label="ffplay helper",tag="Primary Window",width=367,height=500,no_close=True,no_collapse=True):
 	dpg.add_button(label='键盘操作指南',tag='keytip')
 	with dpg.tooltip("keytip"):
 		dpg.add_text(
@@ -76,28 +82,28 @@ with dpg.window(label="ffplay",tag="Primary Window"):
 
 	# 选项
 	with dpg.group():
-		dpg.add_checkbox(label="强制显示宽度",tag="s-x")
+		dpg.add_checkbox(label="强制显示宽度",tag="s_x")
 		dpg.add_input_int(label="^ 宽度",tag="s_width")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="强制显示高度",tag="s-y")
+		dpg.add_checkbox(label="强制显示高度",tag="s_y")
 		dpg.add_input_int(label="^ 高度",tag="s_height")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="禁止音频",tag="s-an")
+		dpg.add_checkbox(label="禁止音频",tag="s_an")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="禁止视频",tag="s-vn")
+		dpg.add_checkbox(label="禁止视频",tag="s_vn")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="跳转到指定的位置(秒)",tag="s-ss")
+		dpg.add_checkbox(label="跳转到指定的位置(秒)",tag="s_ss")
 		dpg.add_input_float(label="^ 位置",tag="s_pos")
 		dpg.add_text("-----")
 		dpg.add_checkbox(label="禁止图像显示(只输出音频)",tag="-nodisp")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="设置窗口标题(默认为输入文件名)",tag="s-window_title")
+		dpg.add_checkbox(label="设置窗口标题(默认为输入文件名)",tag="s_window_title")
 		dpg.add_input_text(label="^ 窗口标题",tag="s_title")
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="循环播放___次(0将一直循环)",tag="s-loop")
-		dpg.add_input_int(label="^ 循环次数",tag="s_number")
+		dpg.add_checkbox(label="循环播放___次(0将一直循环)",tag="s_loop")
+		s_number=dpg.add_input_int(label="^ 循环次数",tag="s_number",default_value=0)
 		dpg.add_text("-----")
-		dpg.add_checkbox(label="设置显示模式",tag="s-showmode")
+		dpg.add_checkbox(label="设置显示模式",tag="s_showmode")
 		dpg.add_input_int(label="^ 模式",tag="s_mode")
 
 	dpg.add_text("-------------------------")
@@ -106,50 +112,57 @@ with dpg.window(label="ffplay",tag="Primary Window"):
 	with dpg.group():
 		dpg.add_text("指令：")
 		dpg.add_text("...",tag="output")
+		dpg.add_button(label="运行", callback=run_ffplay)
+	
 
-# 生成
-def gen_ins():
-	global ffplay_dict
-	global running_ins
+
+
+dpg.bind_font(default_font)
+
+dpg.create_viewport(title='ffplay helper',width=367,height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+
+while dpg.is_dearpygui_running():
+	# 生成
 	error = False
 
 	running_ins = "ffplay "
 
-	if dpg.get_value("s-x") == True:
-		running_ins += "-x " + str(dpg.get_value("s-width")) + " "
+	if dpg.get_value("s_x") == True:
+		running_ins += "-x " + str(dpg.get_value("s_width")) + " "
 
-	if dpg.get_value("s-y") == True:
-		running_ins += "-x " + str(dpg.get_value("s-height")) + " "
+	if dpg.get_value("s_y") == True:
+		running_ins += "-y " + str(dpg.get_value("s_height")) + " "
 
-	if dpg.get_value("s-an") == True:
+	if dpg.get_value("s_an") == True:
 		running_ins += "-an "
 
-	if dpg.get_value("s-vn") == True:
+	if dpg.get_value("s_vn") == True:
 		running_ins += "-vn "
 
-	if dpg.get_value("s-ss") == True:
-		running_ins += "-ss " + str(dpg.get_value("s-pos")) + " "
+	if dpg.get_value("s_ss") == True:
+		running_ins += "-ss " + str(dpg.get_value("s_pos")) + " "
 
-	if dpg.get_value("s-nodisp") == True:
+	if dpg.get_value("s_nodisp") == True:
 		running_ins += "-nodisp "
-	
-	if dpg.get_value("s-window_title") == True:
-		running_ins += "-x " + dpg.get_value("s-title") + " "
 
-	if dpg.get_value("s-loop") == True:
-		print(dpg.get_value("s-number"))
-		running_ins += "-loop " + str(dpg.get_value("s-number")) + " "
+	if dpg.get_value("s_window_title") == True:
+		running_ins += "-x " + dpg.get_value("s_title") + " "
+	if dpg.get_value("s_loop") == True:
+		#print(dpg.get_value(s_number))
+		running_ins += "-loop " + str(dpg.get_value(s_number)) + " "
 
-	if dpg.get_value("s-showmode") == True:
-		if not( dpg.get_value("s-mode") in [0,1,2] ):
-			#print(dpg.get_value("s-mode"))
-			#print(dpg.get_value("s-mode") in [0,1,2])
+	if dpg.get_value("s_showmode") == True:
+		if not( dpg.get_value("s_mode") in [0,1,2] ):
+			#print(dpg.get_value("s_mode"))
+			#print(dpg.get_value("s_mode") in [0,1,2])
 			error = True
 		else:
-			running_ins += "-showmode " + str(dpg.get_value("s-mode")) + " "
+			running_ins += "-showmode " + str(dpg.get_value("s_mode")) + " "
 
-	if ffplay_dict["path"][1] != " ":
-		running_ins += ffplay_dict["path"][1]
+	if ffplay_dict["path"][1] != "":
+		running_ins += '"' + ffplay_dict["path"][1] + '"'
 	else:
 		error = True
 
@@ -157,19 +170,7 @@ def gen_ins():
 		running_ins = 'error'
 	dpg.set_value("output",value=running_ins)
 
-#dpg.show_debug()
-dpg.show_item_registry()
 
-
-
-dpg.bind_font(default_font)
-
-dpg.create_viewport(title='ffplay helper',width=500,height=500)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-
-while dpg.is_dearpygui_running():
-	gen_ins()
 	dpg.render_dearpygui_frame()
 
 dpg.set_primary_window("Primary Window", True)
